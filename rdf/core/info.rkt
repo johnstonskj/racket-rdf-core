9e83b460310d6dce983df0436ab66506b3d22f4c
Simon Johnston <johnstonskj@gmail.com>
2024-03-25 16:01:04 -0700

    build: fix missing uuid

--- rdf/core/info.rkt
+++ rdf/core/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define name "rdf/core: RDF Core data-model")
 (define deps '("base" "uuid"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "uuid"))
 (define scribblings '(("scribblings/rdf-core.scrbl" ())))
 (define pkg-desc "The RDF data model: resources, statements, graphs, and datasets.")
 (define version "0.1.0")
